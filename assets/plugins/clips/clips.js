70e782671b649645a5cf3b4e45e7953db36d271c
Evgeny Gor <gorchak.evgeny@gmail.com>
2020-04-27 18:52:18 +0300

    fixes;

--- assets/plugins/clips/clips.js
+++ assets/plugins/clips/clips.js
@@ -9,7 +9,7 @@
 					['Lorem ipsum...', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'],
 					['Red label', '<span class="label-red">Label</span>'],
 					['Claim Button', '<a class="btn-dc btn-main" href="/claim">Start your claim</a>'],
-					['Claim Widget with money background', '<div class="c-acf__cta"> <div class="c-acf__cta-money"> <span class="c-acf__cta-text">Data breached? You might have a right to compensation - up to $750</span> <a class="c-acf__cta-button btn-dc" href="/claim">Claim NOW</a> </div> </div>']
+					['Claim Widget with money background', '<div class="c-acf__cta"> <div class="c-acf__cta-money"> <span class="c-acf__cta-text">Data breached? You might have a right to compensation - up to $750</span> <a class="c-acf__cta-button btn-dc" href="/claim">Claim NOW</a> </div> </div>'],
 					['Claim Widget with color background', '<div class="c-acf__cta"> <div class="c-acf__cta-background"> <span class="c-acf__cta-text">Data breached? You might have a right to compensation - up to $750</span> <a class="c-acf__cta-button btn-dc" href="/claim">Claim NOW</a> </div> </div>']
 				];
 
