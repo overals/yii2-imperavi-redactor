1761ac0cd3f28eb290555ecd765d9ac485eecb36
Evgeny Gor <gorchak.evgeny@gmail.com>
2020-04-27 18:09:49 +0300

    added clip for button;

--- assets/plugins/clips/clips.js
+++ assets/plugins/clips/clips.js
@@ -7,7 +7,8 @@
 			{
 				var items = [
 					['Lorem ipsum...', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'],
-					['Red label', '<span class="label-red">Label</span>']
+					['Red label', '<span class="label-red">Label</span>'],
+					['Claim Button', '<a class="btn-dc btn-main" href="/claim">Start your claim</a>']
 				];
 
 				this.clips.template = $('<ul id="redactor-modal-list">');
